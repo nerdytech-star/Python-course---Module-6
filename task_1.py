7210adfda65fd58bde22dae8e9a8f5971ed82ec0
nerdytech-star <khushiakf88@gmail.com>
2025-05-23 13:52:12 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,11 @@
+marks={"alice": 85, "bob": 90, "charlie": 78}
+get_name=input("Enter the name of the student: ")
+if get_name in marks:
+    print(f"The marks of {get_name} is {marks[get_name]}")
+else:
+    print("Student not found")
+# The code above is a simple Python script that checks if a student's name is in the dictionary 'marks' and prints their marks if found.
+    
+
+
+
---
+++ task_2.py
@@ -0,0 +1,6 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+print("original list:", list)
+extracted_list=list[0:5]
+print("extracted first five elements:", extracted_list)
+reversed_list=extracted_list[::-1]
+print("reversed extracted list:", reversed_list)
